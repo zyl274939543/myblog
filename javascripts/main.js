0cbab853a86c2ffb00b43e86d11de1128e4a5411
zyl274939543 <xiaozhou_1990@sina.cn>
2016-10-18 15:52:16 +0800

    add css
    add css

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,9 @@
 window.onload=function(){
 	$(".list-inline li").hover(function(){
 		$(this).addClass("active");
-		$(".line").css("left",$(this).css("left"));
+		$(".line").css("left",$(this).css("left")).css("display": "inline").css("width": "54px");
 	},function(){
 		$(this).removeClass("active");
-		$(".line").css("left",$(".active").css("left"));
+		$(".line").css("left",$(".active").css("left")).css("display": "inline").css("width": "54px");;
 	})
 }
