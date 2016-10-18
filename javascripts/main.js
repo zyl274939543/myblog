9758cdd22d884356eeee47454e95a9372120ccf5
zyl274939543 <xiaozhou_1990@sina.cn>
2016-10-18 15:47:29 +0800

    add js
    add js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+window.onload=function(){
+	$(".list-inline li").hover(function(){
+		$(this).addClass("active");
+		$(this).css("left",active_left);
+	},function(){
+		$(this).removeClass("active");
+		$(".line").css("left",$(".active").css("left"));
+	})
+}
