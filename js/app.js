48ca03e1e02d43f6e562ff4a99422d6cee040974
zyl274939543 <xiaozhou_1990@sina.cn>
2016-10-27 09:47:55 +0800

    add badindex

---
+++ js/app.js
@@ -0,0 +1,50 @@
+require.config({
+    baseUrl: 'js/lib',
+    paths: {
+        bootstrap:'bootstrap.min',
+        npm:'npm',
+        jquery:'jquery-1.9.0.min'
+    }
+});
+require(['jquery'],function($){
+	GetBadList();
+})
+function GetBadList()
+{
+	$.ajax({
+		type:"get",
+		url:"http://cloud.bmob.cn/b042e00844addc94/GetBadInfoList",
+		dataType: 'jsonp',
+		jsonp: "callback",
+        jsonpCallback:"ShowList",
+		success:function(data){
+			alert("1");
+		},
+		error:function(){
+			console.log("error");
+		}
+	});
+}
+function ShowList()
+{
+	alert("1");
+}
+function SetHeadImg(num_id)
+{
+	var id=parseInt(num_id);
+	switch(id)
+	{
+		case 1:return "head.jpg";break;
+		case 2:return "head.jpg";break;
+		case 3:return "head.jpg";break;
+		case 4:return "head.jpg";break;
+		case 5:return "head.jpg";break;
+		default:return "head.jpg";break;
+	}
+}
+
+
+
+
+
+
